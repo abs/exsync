57e34c6eca15363e055b65a977f05905ce64414b
Andrei Soroker <soroker@gmail.com>
2026-02-01 15:06:54 -0800

    Add tests for Config.beam_dirs and src_dirs directory filtering

--- test/exsync_test.exs
+++ test/exsync_test.exs
@@ -1,7 +1,30 @@
-defmodule ExsyncTest do
+defmodule ExSyncTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "ExSync.Config.beam_dirs/0" do
+    test "returns only existing directories" do
+      dirs = ExSync.Config.beam_dirs()
+
+      Enum.each(dirs, fn dir ->
+        assert File.exists?(dir), "Expected #{dir} to exist"
+      end)
+    end
+
+    test "includes the project build path" do
+      dirs = ExSync.Config.beam_dirs()
+      build_path = Mix.Project.build_path()
+
+      assert build_path in dirs
+    end
+  end
+
+  describe "ExSync.Config.src_dirs/0" do
+    test "returns only existing directories" do
+      dirs = ExSync.Config.src_dirs()
+
+      Enum.each(dirs, fn dir ->
+        assert File.exists?(dir), "Expected #{dir} to exist"
+      end)
+    end
   end
 end
